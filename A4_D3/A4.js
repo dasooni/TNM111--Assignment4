203ab0d1e10445173c54082a9cbe25a8f4e85a3b
dasooni <dr.amit@live.se>
2023-02-11 14:15:04 +0100

    hover tooltip

--- A4_D3/A4.js
+++ A4_D3/A4.js
@@ -1,43 +1,80 @@
-// set the dimensions and margins of the graph
-
+// set the dimensions of the canvas
 width = 800;
 height = 800;
 
-const svg = d3.select("#ep1") 
+var transform = d3.zoom()
+    .scaleExtent([0.5, 2])  // This control how much you can unzoom (x0.5) and zoom (x20)
+    .translateExtent([[-100, -100], [width + 90, height + 100]]) // This control how much you can pan
+    .extent([[0, 0], [width, height]]);// This is where the magic happens
+
+
+const svg = d3.select("#graph1") 
     .append("svg")
         .attr("width", width)
         .attr("height", height)
-    .append("g")
-const svg_right = d3.select("#ep_all")
+        .call(d3.zoom().on("zoom", function (event) {
+            transform = event.transform;
+            svg.attr("transform", transform);
+        }))
+        .append("g");
+
+const svg_right = d3.select("#graph2")
     .append("svg")
         .attr("width", width)
-        .attr("height", height)
-    .append("g")
+        .attr("height", height);
+
+        
 
 ep_all = d3.json("/starwars-interactions/starwars-full-interactions-allCharacters.json")
-
-d3.json("/starwars-interactions/starwars-episode-1-interactions-allCharacters.json").then (function (data) {
+ep_all.then (function (data) {
     console.log(data.nodes)
-    const link = svg.selectAll("line")
+    console.log(data.links)
+
+
+    const link = svg.append("g")
+        .attr("stroke", "#999")
+        .attr("stroke-opacity", 0.6)
+        .selectAll("line")
         .data(data.links)
-        .join("line")
-            .style("stroke", "#aaa")
-    
-    
-    const node = svg
+        .enter().append("line")
+        .attr("stroke-width", function(d) {return Math.sqrt(d.value)})
+
+    var tooltip = d3.select("#graph1").append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .text("tooltip")    
+
+    const node = svg.append("g")
+        .attr("class", "nodes")
         .selectAll("circle")
         .data(data.nodes)
-        .join("circle")
-            .attr("r", 20)
-            .style("fill", function(d) {return d.colour})
-        .on("mouseover", function mouseover() {
-            d3.select(this).select("circle").transition()
+        .enter().append("circle")
+        .attr("r", 10)
+        .style("fill", function(d) {return d.colour});
+
+    node.append("title")
+        .text(function(d) {return String (d.name)})
+
+
+    node.on("mouseover", function mouseover(event,d) {
+        // Show d.name in tooltip¨
+
+        tooltip.transition()
+            .duration(200)
+            .style("opacity", 1);
+        tooltip
+            .html(d.name)
+            .style("left", (event.pageX + 10) + "px")
+            .style("top", (event.pageY - 28) + "px")
+    });
+
+    node.on("mouseout", function mouseout(d) {
+        tooltip.transition()
             .duration(500)
-            console.log (function(d) {return String (d.name)})
-
-        })
-
-
+            .style("opacity", 0);   
+        });
+        
+    
     const simulation = d3.forceSimulation(data.nodes)
         .force('link', d3.forceLink()
             .id(function(d) {return d.index})
@@ -45,7 +82,7 @@ d3.json("/starwars-interactions/starwars-episode-1-interactions-allCharacters.js
         )
         .force("charge", d3.forceManyBody().strength(-400))
         .force("center", d3.forceCenter(width/2, height/2))
-        .on("end", ticked)
+        .on("tick", ticked)
 
     function ticked() {
 
@@ -56,12 +93,36 @@ d3.json("/starwars-interactions/starwars-episode-1-interactions-allCharacters.js
         .attr("y2", function(d) {return d.target.y})
         
         node
-            .attr("cx", function (d) {return d.x+6})
-            .attr("cy", function (d) {return d.y-6})
+        .attr("cx", function (d) {return d.x})
+        .attr("cy", function (d) {return d.y})
 
-    
     }
+
+    function dragstarted(event, d) {
+        if (!event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+    }
+
+    function dragged(event, d) {
+        d.fx = event.x;
+        d.fy = event.y;
+    }
+
+    function dragended(event, d) {
+        if (!event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+    }
+
+    node.call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended));
+
 })
+
+
 
 
 
